24549ff7a9d5634af84a93ec7137b0531f643035
praveenmitian <praveenmitian@gmail.com>
2019-09-24 13:49:05 +0530

    Update for error

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ INSERT into users (username, password, enabled) values ('user', 'pass', true);
 INSERT into users (username, password, enabled) values ('admin', 'pass', true);
 
 INSERT into authorities (username, authority) values ('user', 'ROLE_USER');
-INSERT into authorities (username, authority) values ('user', 'ROLE_ADMIN');
+INSERT into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
