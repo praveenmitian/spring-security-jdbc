8bf2de5b76b11029b8938c29d27c2ccc82dfa6ac
praveenmitian <praveenmitian@gmail.com>
2019-09-24 13:43:39 +0530

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT into users (username, password, enabled) values ('user', 'pass', true);
+INSERT into users (username, password, enabled) values ('admin', 'pass', true);
+
+INSERT into authorities (username, authority) values ('user', 'ROLE_USER');
+INSERT into authorities (username, authority) values ('user', 'ROLE_ADMIN');
